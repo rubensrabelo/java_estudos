2485a19a7529608d0bf901fc8079ae154c0c8007
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-06-05 11:37:47 -0300

    Currency Converter exercise

--- curso_nelio_alves/secao_08/model_05_currency_converter/src/model/entities/CurrencyConverter.java
+++ curso_nelio_alves/secao_08/model_05_currency_converter/src/model/entities/CurrencyConverter.java
@@ -1,5 +1,44 @@
 package model.entities;
 
 public class CurrencyConverter {
+	public static final double IOF = 0.06;
+	
+	private double dollarPrice;
+	private double boughtDollar;
+	
+	public CurrencyConverter() {
+	}
 
+	public CurrencyConverter(double boughtDollar, double dollarPrice) {
+		this.boughtDollar = boughtDollar;
+		this.dollarPrice = dollarPrice;
+	}
+
+	public double getBoughtDollar() {
+		return boughtDollar;
+	}
+
+	public void setBoughtDollar(double boughtDollar) {
+		this.boughtDollar = boughtDollar;
+	}
+	
+	public double getDollarPrice() {
+		return this.dollarPrice;
+	}
+
+	public void setDollarPrice(double dollarPrice) {
+		this.dollarPrice = dollarPrice;
+	}
+	
+	public double paymentInReais() {
+		double result = getDollarPrice() * getBoughtDollar();
+		result += result * IOF;
+		
+		return result;
+	}
+	
+	@Override
+	public String toString() {
+		return "Amount to be paid in reais = " + String.format("%.2f", paymentInReais());
+	}
 }
