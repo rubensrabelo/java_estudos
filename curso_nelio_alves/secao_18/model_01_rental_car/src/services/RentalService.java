c62de78c3ed7c9353c3f729f37d9a55621f1dde9
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-06-23 09:29:17 -0300

    Rental Car

--- curso_nelio_alves/secao_18/model_01_rental_car/src/services/RentalService.java
+++ curso_nelio_alves/secao_18/model_01_rental_car/src/services/RentalService.java
@@ -1,5 +1,23 @@
 package services;
 
+import java.time.Duration;
+
+import entities.CarRental;
+
 public class RentalService {
+	private Double pricePerhour;
+	private Double pricePerDay;
 	
+	private TaxService taxService;
+	
+	public RentalService(Double pricePerhour, Double pricePerDay, TaxService taxService) {
+		this.pricePerhour = pricePerhour;
+		this.pricePerDay = pricePerDay;
+		this.taxService = taxService;
+	}
+
+	public void processInvoice(CarRental car) {
+		Duration duration = Duration.between(car.getStart(), car.getFinish());
+		long hour = duration.getSeconds() / 3600;
+	}
 }
