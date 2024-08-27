2e4d1a3dae7afe6187a6d7f4dc028db4d439ed70
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-08-27 08:18:58 -0300

    Understing path params with spring boot

---
+++ firstProject/src/main/java/com/course/project/firstProject/MathController.java
@@ -0,0 +1,18 @@
+package com.course.project.firstProject;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+	
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double sum(
+				@PathVariable(value = "numberOne") String numberOne,
+				@PathVariable(value = "numberTwo") String numberTwo
+			) {
+		return 1D;
+	}
+}
