2f46f0b9f3cddbb94118aeb04cf78b0143e54585
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-09-08 13:41:19 -0300

    Creating methods http

--- projetos/registerUsers/src/main/java/com/registerUsers/registerUsers/resources/UserResource.java
+++ projetos/registerUsers/src/main/java/com/registerUsers/registerUsers/resources/UserResource.java
@@ -1,10 +1,17 @@
 package com.registerUsers.registerUsers.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.registerUsers.registerUsers.models.User;
 import com.registerUsers.registerUsers.services.UserService;
@@ -18,9 +25,33 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
+	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
 		List<User> users = service.findAll();
 		
 		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable("id") Long id){
+		User user = service.findById(id);
+		
+		return ResponseEntity.ok().body(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user) {
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(user.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(user);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
--- projetos/registerUsers/src/main/java/com/registerUsers/registerUsers/services/UserService.java
+++ projetos/registerUsers/src/main/java/com/registerUsers/registerUsers/services/UserService.java
@@ -1,12 +1,17 @@
 package com.registerUsers.registerUsers.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.registerUsers.registerUsers.models.User;
 import com.registerUsers.registerUsers.repositories.UserRepository;
+import com.registerUsers.registerUsers.services.exceptions.DatabaseException;
+import com.registerUsers.registerUsers.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +21,25 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> userOpt = repository.findById(id);
+		
+		return userOpt.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public User insert(User user) {
+		return repository.save(user);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 }
