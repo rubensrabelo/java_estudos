f3608a34f297cc7ee0ae7d63be6bff4f3523cbb5
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-09-09 16:33:52 -0300

    Starting the product management project

---
+++ projetos/productManagement/src/test/java/com/productManagement/productManagement/startupTests.java
@@ -0,0 +1,13 @@
+package com.productManagement.productManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class startupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
