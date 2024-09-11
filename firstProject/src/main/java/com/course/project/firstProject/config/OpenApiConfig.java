856dbb85dd581c0e4ffa28b825edba21a38313b0
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-09-11 10:15:54 -0300

    Adding more information

--- firstProject/src/main/java/com/course/project/firstProject/config/OpenApiConfig.java
+++ firstProject/src/main/java/com/course/project/firstProject/config/OpenApiConfig.java
@@ -1,5 +1,24 @@
 package com.course.project.firstProject.config;
 
-public class OpenApiConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+	
+	@Bean
+	public OpenAPI customOpenApi() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Restful API with Spring Boot 3")
+						.version("v1")
+						.description("")
+						.termsOfService("")
+						.license(new License().name("apache 2.0")
+								.url("")));
+	}
 }
