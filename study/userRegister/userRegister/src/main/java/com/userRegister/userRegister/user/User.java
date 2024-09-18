9638a7472065db6c402d14d2366b45c882bfb876
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-09-18 10:41:47 -0300

    Adding databases and entities

---
+++ study/userRegister/userRegister/src/main/java/com/userRegister/userRegister/user/User.java
@@ -0,0 +1,35 @@
+package com.userRegister.userRegister.user;
+
+import java.util.List;
+
+import com.userRegister.userRegister.role.Role;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "USERS")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String username;
+	private String password;
+	
+	@ManyToMany
+	private List<Role> roles;
+}
