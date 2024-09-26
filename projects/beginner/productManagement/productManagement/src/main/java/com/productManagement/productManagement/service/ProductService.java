3ea4b0d8d9de69e37ea2e7966544fac82490a441
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-09-26 16:39:03 -0300

    Finalizing product service

--- projects/beginner/productManagement/productManagement/src/main/java/com/productManagement/productManagement/service/ProductService.java
+++ projects/beginner/productManagement/productManagement/src/main/java/com/productManagement/productManagement/service/ProductService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
 
 import com.productManagement.productManagement.models.Product;
 import com.productManagement.productManagement.repositories.ProductRepository;
@@ -14,6 +15,7 @@ import com.productManagement.productManagement.service.exceptions.ResourceNotFou
 
 import jakarta.persistence.EntityNotFoundException;
 
+@Service
 public class ProductService {
 	
 	@Autowired
