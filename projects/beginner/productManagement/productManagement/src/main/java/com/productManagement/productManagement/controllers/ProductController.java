5bd63f45191189f5ffa6dee86df1fe989bd0bdca
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-09-29 09:29:22 -0300

    Adding filter by name and category in the product controller

--- projects/beginner/productManagement/productManagement/src/main/java/com/productManagement/productManagement/controllers/ProductController.java
+++ projects/beginner/productManagement/productManagement/src/main/java/com/productManagement/productManagement/controllers/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -37,6 +38,30 @@ public class ProductController {
 		Product product = service.findById(id);
 		
 		return ResponseEntity.ok().body(product);
+	}
+	
+	@GetMapping(value = "/name")
+	public ResponseEntity<List<Product>> findByName(@RequestParam(value = "name") String name) {
+		List<Product> products = service.findByName(name);
+		
+		return ResponseEntity.ok().body(products);
+	}
+	
+	@GetMapping(value = "/category")
+	public ResponseEntity<List<Product>> findByCategory(@RequestParam(value = "category") String category) {
+		List<Product> products = service.findByCategory(category);
+		
+		return ResponseEntity.ok().body(products);
+	}
+	
+	@GetMapping(value = "/search")
+	public ResponseEntity<List<Product>> findByNameAndCategory(
+				@RequestParam(value = "name") String name,
+				@RequestParam(value = "category") String category
+			) {
+		List<Product> products = service.findByNameAndCategory(name, category);
+		
+		return ResponseEntity.ok().body(products);
 	}
 	
 	@PostMapping
