9b60e0102f2fbd936dde23acb2591e24fdc88078
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-09-29 07:57:06 -0300

    Section 7 http method

--- REST_API's_RESTFul_do_0_a_AWS_c._Spring_Boot_3_Java_e_Docker/course/course/src/main/java/com/course/course/services/PersonService.java
+++ REST_API's_RESTFul_do_0_a_AWS_c._Spring_Boot_3_Java_e_Docker/course/course/src/main/java/com/course/course/services/PersonService.java
@@ -1,5 +1,7 @@
 package com.course.course.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,10 +15,34 @@ public class PersonService {
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonService.class.getName());
 	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<>();
+		
+		logger.info("Finding all people!");
+		
+		return persons;
+	}
+	
 	public Person findById(String id) {
 		
 		logger.info("Finding one Person!");
 		
 		return new Person();
+	}
+	
+	public Person create(Person person) {
+		logger.info("Creating one person!");
+		
+		return person;
+	}
+	
+	public Person update(Person person) {
+		logger.info("Updating one person!");
+		
+		return person;
+	}
+	
+	public void delete(String id) {
+		logger.info("Deleting one person!");
 	}
 }
