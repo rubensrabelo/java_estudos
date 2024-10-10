0d2f84ac4e28cd1192f4d6986e2073719b54232b
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-10-10 08:44:30 -0300

    Finishing unit test with dozer mapper

--- projects/beginner/todolist/todolist/src/test/java/com/project/todolist/mapper/DozerConverterTest.java
+++ projects/beginner/todolist/todolist/src/test/java/com/project/todolist/mapper/DozerConverterTest.java
@@ -129,6 +129,59 @@ public class DozerConverterTest {
 
 	@Test
 	public void parseVOListToEntityList() {
+		List<Task> tasks = DozerMapper.parseListObjects(inputObjects.mockVOList(8), Task.class);
 
+		Task taskOne = tasks.get(1);
+
+		assertEquals(Long.valueOf(1L), taskOne.getId());
+		assertEquals("Task 1", taskOne.getName());
+		assertEquals("Description 1", taskOne.getDescription());
+		assertEquals(TaskStatus.PENDING, taskOne.getTaskStatus());
+
+		assertNotNull(taskOne.getCreatedAt());
+		assertNotNull(taskOne.getUpdatedAt());
+
+		LocalDateTime now = LocalDateTime.now();
+
+		assertTrue(taskOne.getCreatedAt().isBefore(now.plusSeconds(1))
+				&& taskOne.getCreatedAt().isAfter(now.minusSeconds(1)));
+		assertTrue(taskOne.getUpdatedAt().isBefore(now.plusSeconds(1))
+				&& taskOne.getUpdatedAt().isAfter(now.minusSeconds(1)));
+
+		assertEquals(taskOne.getCreatedAt(), taskOne.getUpdatedAt());
+
+		Task taskSix = tasks.get(6);
+
+		assertEquals(Long.valueOf(6L), taskSix.getId());
+		assertEquals("Task 6", taskSix.getName());
+		assertEquals("Description 6", taskSix.getDescription());
+		assertEquals(TaskStatus.COMPLETED, taskSix.getTaskStatus());
+
+		assertNotNull(taskSix.getCreatedAt());
+		assertNotNull(taskSix.getUpdatedAt());
+
+		assertTrue(taskSix.getCreatedAt().isBefore(now.plusSeconds(1))
+				&& taskSix.getCreatedAt().isAfter(now.minusSeconds(1)));
+		assertTrue(taskSix.getUpdatedAt().isBefore(now.plusSeconds(1))
+				&& taskSix.getUpdatedAt().isAfter(now.minusSeconds(1)));
+
+		assertEquals(taskSix.getCreatedAt(), taskSix.getUpdatedAt());
+
+		Task taskSeven = tasks.get(7);
+
+		assertEquals(Long.valueOf(7L), taskSeven.getId());
+		assertEquals("Task 7", taskSeven.getName());
+		assertEquals("Description 7", taskSeven.getDescription());
+		assertEquals(TaskStatus.PENDING, taskSeven.getTaskStatus());
+
+		assertNotNull(taskSeven.getCreatedAt());
+		assertNotNull(taskSeven.getUpdatedAt());
+
+		assertTrue(taskSeven.getCreatedAt().isBefore(now.plusSeconds(1))
+				&& taskSeven.getCreatedAt().isAfter(now.minusSeconds(1)));
+		assertTrue(taskSeven.getUpdatedAt().isBefore(now.plusSeconds(1))
+				&& taskSeven.getUpdatedAt().isAfter(now.minusSeconds(1)));
+
+		assertEquals(taskSeven.getCreatedAt(), taskSeven.getUpdatedAt());
 	}
 }
