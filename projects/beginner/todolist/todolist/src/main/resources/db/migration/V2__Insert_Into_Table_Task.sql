fa53c7b2d0ff3cbf68d4550c687ef3922c4c8061
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-10-11 12:53:26 -0300

    Adding migrations with flyway

---
+++ projects/beginner/todolist/todolist/src/main/resources/db/migration/V2__Insert_Into_Table_Task.sql
@@ -0,0 +1,16 @@
+INSERT INTO public.task (id, name, description, task_status, created_at, updated_at) VALUES
+(1, 'Exercícios', 'Exercícios', 1, '2024-10-01 09:00:00', '2024-10-01 09:00:00'),
+(2, 'Bíblia', 'Bíblia', 1, '2024-10-01 09:30:00', '2024-10-01 09:30:00'),
+(3, 'Java', 'Java', 2, '2024-10-01 10:00:00', '2024-10-01 10:00:00'),
+(4, 'Java - Projeto', 'Java - Projeto', 1, '2024-10-02 10:30:00', '2024-10-02 10:30:00'),
+(5, 'Materias da Faculdade', 'Materias da Faculdade', 2, '2024-10-02 11:00:00', '2024-10-02 11:00:00'),
+(6, 'HTML / CSS', 'HTML / CSS', 1, '2024-10-02 11:30:00', '2024-10-02 11:30:00'),
+(7, 'Livro', 'Livro', 3, '2024-10-03 12:00:00', '2024-10-03 12:00:00'),
+(8, 'Vagas', 'Vagas', 1, '2024-10-03 12:30:00', '2024-10-03 12:30:00'),
+(9, 'Inglês (Básico)', 'Inglês (Básico)', 2, '2024-10-03 13:00:00', '2024-10-03 13:00:00'),
+(10, 'Programação Orientada a Objetos', 'POO', 1, '2024-10-04 14:00:00', '2024-10-04 14:00:00'),
+(11, 'Análise de Dados', 'Análise de Dados', 2, '2024-10-04 14:30:00', '2024-10-04 14:30:00'),
+(12, 'Projetos de Desenvolvimento', 'Projetos', 1, '2024-10-04 15:00:00', '2024-10-04 15:00:00'),
+(13, 'Trabalho em Grupo', 'Grupo', 3, '2024-10-05 16:00:00', '2024-10-05 16:00:00'),
+(14, 'Estudo de Caso', 'Estudo de Caso', 1, '2024-10-05 16:30:00', '2024-10-05 16:30:00'),
+(15, 'Preparação para Entrevista', 'Entrevista', 2, '2024-10-05 17:00:00', '2024-10-05 17:00:00');
