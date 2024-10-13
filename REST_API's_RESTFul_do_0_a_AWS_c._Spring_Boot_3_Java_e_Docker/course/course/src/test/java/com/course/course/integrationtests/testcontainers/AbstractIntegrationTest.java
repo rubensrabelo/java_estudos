ea5c7810969a3c54563a4fe65642038f8f415f21
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-10-13 07:23:21 -0300

    Section 15

--- REST_API's_RESTFul_do_0_a_AWS_c._Spring_Boot_3_Java_e_Docker/course/course/src/test/java/com/course/course/integrationtests/testcontainers/AbstractIntegrationTest.java
+++ REST_API's_RESTFul_do_0_a_AWS_c._Spring_Boot_3_Java_e_Docker/course/course/src/test/java/com/course/course/integrationtests/testcontainers/AbstractIntegrationTest.java
@@ -14,7 +14,7 @@ import org.testcontainers.lifecycle.Startables;
 @ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
 public class AbstractIntegrationTest {
 	
-	public class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+	static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 		static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.29");
 		
 		private static void startContainers() {
