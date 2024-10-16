bbc497f90b463721fceecaf4c0ef2c604238b2a0
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-10-16 08:40:13 -0300

    Testing exceptions in the update and insert methods

--- projects/beginner/todolist/todolist/src/main/java/com/project/todolist/services/exceptions/DatabaseException.java
+++ projects/beginner/todolist/todolist/src/main/java/com/project/todolist/services/exceptions/DatabaseException.java
@@ -1,5 +1,9 @@
 package com.project.todolist.services.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class DatabaseException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
--- projects/beginner/todolist/todolist/src/test/java/com/project/todolist/unittests/mockito/services/TaskServiceTest.java
+++ projects/beginner/todolist/todolist/src/test/java/com/project/todolist/unittests/mockito/services/TaskServiceTest.java
@@ -1,5 +1,8 @@
 package com.project.todolist.unittests.mockito.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.when;
 
@@ -15,6 +18,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.project.todolist.data.vo.v1.TaskVO;
+import com.project.todolist.enums.TaskStatus;
 import com.project.todolist.models.Task;
 import com.project.todolist.repositories.TaskRepository;
 import com.project.todolist.services.TaskService;
@@ -23,15 +28,15 @@ import com.project.todolist.unittest.mapper.mocks.MockTask;
 @TestInstance(Lifecycle.PER_CLASS)
 @ExtendWith(MockitoExtension.class)
 class TaskServiceTest {
-	
+
 	MockTask input;
-	
+
 	@InjectMocks
 	private TaskService service;
-	
+
 	@Mock
 	TaskRepository repository;
-	
+
 	@BeforeEach
 	void setUpMocks() throws Exception {
 		input = new MockTask();
@@ -45,12 +50,22 @@ class TaskServiceTest {
 
 	@Test
 	void testFindById() {
-		Task task = input.mockEntity();
-		task.setId(1L);
-		
-		when(repository.findById(1L)).thenReturn(Optional.of(task));
-		
+		Task entity = input.mockEntity(1);
+		entity.setId(1L);
+
+		when(repository.findById(1L)).thenReturn(Optional.of(entity));
+
 		var result = service.findById(1L);
+
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		assertNotNull(result.getLinks());
+		
+		assertTrue(result.toString().contains("links: [</api/tasks/v1/1>;rel=\"self\"]"));
+
+		assertEquals("Task 1", result.getName());
+		assertEquals("Description 1", result.getDescription());
+		assertEquals(TaskStatus.PENDING, result.getTaskStatus());
 	}
 
 	@Test
@@ -60,17 +75,62 @@ class TaskServiceTest {
 
 	@Test
 	void testInsert() {
-		fail("Not yet implemented");
+		Task entity = input.mockEntity(1);
+
+		Task persisted = input.mockEntity(1);
+		persisted.setId(1L);
+
+		TaskVO vo = input.mockVO(1);
+		vo.setKey(1L);
+		
+		when(repository.findById(1L)).thenReturn(Optional.of(entity));
+		when(repository.save(entity)).thenReturn(persisted);
+
+		var result = service.update(1L, vo);
+
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		assertNotNull(result.getLinks());
+		
+		assertTrue(result.toString().contains("links: [</api/tasks/v1/1>;rel=\"self\"]"));
+
+		assertEquals("Task 1", result.getName());
+		assertEquals("Description 1", result.getDescription());
+		assertEquals(TaskStatus.PENDING, result.getTaskStatus());
 	}
 
 	@Test
 	void testDelete() {
-		fail("Not yet implemented");
+		Task entity = input.mockEntity(1);
+		entity.setId(1L);
+
+		service.delete(1L);
 	}
 
 	@Test
 	void testUpdate() {
-		fail("Not yet implemented");
+		Task entity = input.mockEntity(1);
+		entity.setId(1L);
+
+		Task persisted = input.mockEntity(1);
+		persisted.setId(1L);
+
+		TaskVO vo = input.mockVO(1);
+		vo.setKey(1L);
+
+		when(repository.save(entity)).thenReturn(persisted);
+
+		var result = service.insert(vo);
+
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		assertNotNull(result.getLinks());
+		
+		assertTrue(result.toString().contains("links: [</api/tasks/v1/1>;rel=\"self\"]"));
+
+		assertEquals("Task 1", result.getName());
+		assertEquals("Description 1", result.getDescription());
+		assertEquals(TaskStatus.PENDING, result.getTaskStatus());
 	}
 
 }
