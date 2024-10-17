a873586b8ed3e614e996bc474f793a4d6653292b
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-10-17 19:30:36 -0300

    Adding cors request

--- projects/beginner/todolist/todolist/src/main/java/com/project/todolist/config/WebConfig.java
+++ projects/beginner/todolist/todolist/src/main/java/com/project/todolist/config/WebConfig.java
@@ -2,10 +2,12 @@ package com.project.todolist.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.project.todolist.serialization.converter.YamlJackson2HttpMessageConverter;
@@ -15,12 +17,24 @@ public class WebConfig implements WebMvcConfigurer {
 	
 	private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
 	
+	@Value("${cors.originPatterns:default}")
+	private String corsOriginPatterns = "";
 	
 	@Override
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(new YamlJackson2HttpMessageConverter());
 	}
 	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		var allowedOrigins = corsOriginPatterns.split(",");
+		
+		registry.addMapping("/**")
+			.allowedMethods("*")
+			.allowedOrigins(allowedOrigins)
+		.allowCredentials(true);
+	}
+
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 		configurer.favorParameter(false).ignoreAcceptHeader(false)
