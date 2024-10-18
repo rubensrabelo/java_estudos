fc0da2b40dd0de9b020b8297235303414cc5ed30
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-10-18 08:28:28 -0300

    Saving cors test

--- projects/beginner/todolist/todolist/src/test/java/com/project/todolist/integrationtests/controller/withjson/TaskControllerJsonTest.java
+++ projects/beginner/todolist/todolist/src/test/java/com/project/todolist/integrationtests/controller/withjson/TaskControllerJsonTest.java
@@ -3,6 +3,8 @@ package com.project.todolist.integrationtests.controller.withjson;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
@@ -10,12 +12,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.todolist.configs.TestConfigs;
 import com.project.todolist.integrationtests.testcontainers.AbstractIntegrationTest;
 import com.project.todolist.integrationtests.vo.TaskVO;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.RequestSpecification;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -37,25 +45,38 @@ public class TaskControllerJsonTest extends AbstractIntegrationTest {
 	
 	@Test
 	@Order(1)
-	public void testeCreate() {
+	public void testeCreate() throws JsonMappingException, JsonProcessingException {
 		mockTask();
 		
-		var content = 
-				given()
-					.basePath("/swagger-ui/index.html")
-					.port(TestConfigs.SERVER_PORT)
-					.when()
-						.get()
-					.then()
-						.statusCode(200)
-					.extract()
-						.body()
-							.asString();
+		especification = new RequestSpecBuilder()
+				.addHeader(TestConfigs.HEADER_PARAM_ORIGIN, "http://localhost:8080")
+				.setBasePath("/api/tasks/v1")
+				.setPort(TestConfigs.SERVER_PORT)
+					.addFilter(new RequestLoggingFilter(LogDetail.ALL))
+					.addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+				.build();
+		
+		var content = given().spec(especification)
+						.contentType(TestConfigs.CONTENT_TYPE_JSON)
+							.body(task)
+							.when()
+							.post()
+						.then()
+							.statusCode(200)
+						.extract()
+							.body()
+								.asString();
+		
+		TaskVO created = objectMapper.readValue(content, TaskVO.class);
+		
 		assertTrue(content.contains("Swagger UI"));
 	}
 
 	private void mockTask() {
-		
+		task.setName("Excel");
+		task.setDescription("Estudar a ferramenta excel");
+		task.setCreatedAt(LocalDateTime.now());
+		task.setUpdatedAt(LocalDateTime.now());
 	}
 
 }
