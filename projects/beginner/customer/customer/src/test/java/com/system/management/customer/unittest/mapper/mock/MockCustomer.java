dbd7e0a56b8f93844ab4e9bd9a37496211fa86c1
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-10-20 19:14:12 -0300

    Editing mock customer

--- projects/beginner/customer/customer/src/test/java/com/system/management/customer/unittest/mapper/mock/MockCustomer.java
+++ projects/beginner/customer/customer/src/test/java/com/system/management/customer/unittest/mapper/mock/MockCustomer.java
@@ -1,5 +1,23 @@
 package com.system.management.customer.unittest.mapper.mock;
 
+import com.system.management.customer.data.vo.CustomerVO;
+import com.system.management.customer.model.Customer;
+
 public class MockCustomer {
 
+	public Customer mockEntity() {
+		return createMockEntity();
+	}
+	
+	public CustomerVO mockVO() {
+		return createMockVO();
+	}
+	
+	public Customer createMockEntity() {
+		return null;
+	}
+	
+	private CustomerVO createMockVO() {
+		return null;
+	}
 }
