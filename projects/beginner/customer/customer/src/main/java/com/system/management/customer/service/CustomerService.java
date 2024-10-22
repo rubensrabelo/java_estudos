3b08ece9966df432aed4c9a536750d036592e164
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-10-22 15:56:04 -0300

    Creating findAll and findById methods in the customer service

--- projects/beginner/customer/customer/src/main/java/com/system/management/customer/service/CustomerService.java
+++ projects/beginner/customer/customer/src/main/java/com/system/management/customer/service/CustomerService.java
@@ -1,4 +1,32 @@
 package com.system.management.customer.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.system.management.customer.data.vo.CustomerVO;
+import com.system.management.customer.mapper.DozerMapper;
+import com.system.management.customer.repository.CustomerRepository;
+import com.system.management.customer.service.exceptions.ResourceNotFoundException;
+
+@Service
 public class CustomerService {
+	
+	@Autowired
+	private CustomerRepository repository;
+	
+	public List<CustomerVO> findAll() {
+		var listVO = DozerMapper.parseListObjects(repository.findAll(), CustomerVO.class);
+		
+		return listVO;
+	}
+	
+	public CustomerVO findById(Long id) {
+		var entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+		
+		var vo = DozerMapper.parseObject(entity, CustomerVO.class);
+		
+		return vo;
+	}
 }
