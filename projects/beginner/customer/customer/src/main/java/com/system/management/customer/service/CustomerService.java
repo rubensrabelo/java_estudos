caa72e8b979e0f00ae9512efc9bda3f1aa6b3467
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-10-23 14:04:11 -0300

    Finishing customer service

--- projects/beginner/customer/customer/src/main/java/com/system/management/customer/service/CustomerService.java
+++ projects/beginner/customer/customer/src/main/java/com/system/management/customer/service/CustomerService.java
@@ -3,12 +3,18 @@ package com.system.management.customer.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.system.management.customer.data.vo.CustomerVO;
 import com.system.management.customer.mapper.DozerMapper;
+import com.system.management.customer.model.Customer;
 import com.system.management.customer.repository.CustomerRepository;
+import com.system.management.customer.service.exceptions.DatabaseException;
+import com.system.management.customer.service.exceptions.RequiredObjectIsNullException;
 import com.system.management.customer.service.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CustomerService {
@@ -28,5 +34,52 @@ public class CustomerService {
 		var vo = DozerMapper.parseObject(entity, CustomerVO.class);
 		
 		return vo;
+	}
+	
+	public CustomerVO create(CustomerVO customer) {
+		if(customer == null)
+			throw new RequiredObjectIsNullException();
+		
+		var entity = DozerMapper.parseObject(customer, Customer.class);
+		
+		var vo = DozerMapper.parseObject(repository.save(entity), CustomerVO.class);
+		
+		return vo;
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (ResourceNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public CustomerVO update(Long id, CustomerVO customer) {
+		if(customer == null)
+			throw new RequiredObjectIsNullException();
+		
+		try {
+			var entity = repository.findById(id).get();
+			
+			var customEntity = DozerMapper.parseObject(customer, Customer.class);
+			
+			updateData(entity, customEntity);
+			
+			return null;
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Customer entity, Customer customEntity) {
+		entity.setFirstName(customEntity.getFirstName());
+		entity.setLastName(customEntity.getLastName());
+		entity.setEmail(customEntity.getEmail());
+		entity.setPhoneNumber(customEntity.getPhoneNumber());
+		entity.setBirthDate(customEntity.getBirthDate());
+		entity.setGender(customEntity.getGender());
 	}
 }
