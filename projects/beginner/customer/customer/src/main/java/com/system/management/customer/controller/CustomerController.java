afe4e4c690f541019a735e6ee000a0bcfa7bbc77
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-10-24 14:38:24 -0300

    Finishing customer controller

--- projects/beginner/customer/customer/src/main/java/com/system/management/customer/controller/CustomerController.java
+++ projects/beginner/customer/customer/src/main/java/com/system/management/customer/controller/CustomerController.java
@@ -1,13 +1,18 @@
 package com.system.management.customer.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.system.management.customer.data.vo.CustomerVO;
 import com.system.management.customer.service.CustomerService;
@@ -29,6 +34,30 @@ public class CustomerController {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<CustomerVO> findById(@PathVariable("id") Long id) {
 		CustomerVO customer = service.findById(id);
+		
+		return ResponseEntity.ok().body(customer);
+	}
+	
+	@PutMapping
+	public ResponseEntity<CustomerVO> create(@RequestBody CustomerVO customer) {
+		customer = service.create(customer);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+				.path("/{id}").buildAndExpand(customer.getKey()).toUri();
+		
+		return ResponseEntity.created(uri).body(customer);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CustomerVO> update(@PathVariable("id") Long id, @RequestBody CustomerVO customer) {
+		customer = service.update(id, customer);
 		
 		return ResponseEntity.ok().body(customer);
 	}
