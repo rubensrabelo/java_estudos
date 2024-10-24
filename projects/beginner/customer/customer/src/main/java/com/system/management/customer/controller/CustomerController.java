d9497adcc846b03fd7c15312e6db2707a52b78a9
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-10-24 16:04:48 -0300

    Fixing put in the controller

--- projects/beginner/customer/customer/src/main/java/com/system/management/customer/controller/CustomerController.java
+++ projects/beginner/customer/customer/src/main/java/com/system/management/customer/controller/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,7 +39,7 @@ public class CustomerController {
 		return ResponseEntity.ok().body(customer);
 	}
 	
-	@PutMapping
+	@PostMapping
 	public ResponseEntity<CustomerVO> create(@RequestBody CustomerVO customer) {
 		customer = service.create(customer);
 		
