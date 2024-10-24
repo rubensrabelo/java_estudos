f5d4378c043b190871cb29362d2b095727f8a9e7
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-10-24 13:31:47 -0300

    section 18

--- REST_API's_RESTFul_do_0_a_AWS_c._Spring_Boot_3_Java_e_Docker/course/course/src/test/java/com/course/course/integrationtests/controller/withjson/PersonControllerJsonTest.java
+++ REST_API's_RESTFul_do_0_a_AWS_c._Spring_Boot_3_Java_e_Docker/course/course/src/test/java/com/course/course/integrationtests/controller/withjson/PersonControllerJsonTest.java
@@ -13,6 +13,8 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.course.course.config.TestConfigs;
+import com.course.course.data.vo.v1.security.AccountCredentialsVO;
+import com.course.course.data.vo.v1.security.TokenVO;
 import com.course.course.integrationtests.testcontainers.AbstractIntegrationTest;
 import com.course.course.integrationtests.vo.PersonVO;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -44,19 +46,40 @@ public class PersonControllerJsonTest extends AbstractIntegrationTest{
 	}
 	
 	@Test
+	@Order(0)
+	public void authorizationTest() throws JsonMappingException, JsonProcessingException {
+		AccountCredentialsVO user = new AccountCredentialsVO("leandro", "admin123");
+		
+		var accessToken = given()
+				.basePath("/auth/signin")
+					.port(TestConfigs.SERVER_PORT)
+					.contentType(TestConfigs.CONTENT_TYPE_JSON)
+				.body(user)
+					.when()
+				.post()
+					.then()
+					.statusCode(200)
+						.extract()
+						.body()
+							.as(TokenVO.class)
+						.getAccessToken();
+		
+		specification = new RequestSpecBuilder()
+				.addHeader(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + accessToken)
+				.setBasePath("/api/person/v1")
+				.setPort(TestConfigs.SERVER_PORT)
+					.addFilter(new RequestLoggingFilter(LogDetail.ALL))
+					.addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+				.build();
+	}
+	
+	@Test
 	@Order(1)
 	public void testCreate() throws JsonMappingException, JsonProcessingException {
 		mockPerson();
 		
-		specification = new RequestSpecBuilder()
-			.addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_ERUDIO)
-			.setBasePath("/api/person/v1")
-			.setPort(TestConfigs.SERVER_PORT)
-				.addFilter(new RequestLoggingFilter(LogDetail.ALL))
-				.addFilter(new ResponseLoggingFilter(LogDetail.ALL))
-			.build();
-		
 		var content = given().spec(specification)
+				.header(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_ERUDIO)
 				.contentType(TestConfigs.CONTENT_TYPE_JSON)
 					.body(person)
 					.when()
