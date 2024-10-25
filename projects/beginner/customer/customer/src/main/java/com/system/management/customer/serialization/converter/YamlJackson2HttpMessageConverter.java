696e423f7532ea0c43a8cce4a9c16fa212aa88b4
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-10-25 09:08:38 -0300

    Adding configuration for content negotiation and fixing YamlJackson2HttpMessageConverter constructor

--- projects/beginner/customer/customer/src/main/java/com/system/management/customer/serialization/converter/YamlJackson2HttpMessageConverter.java
+++ projects/beginner/customer/customer/src/main/java/com/system/management/customer/serialization/converter/YamlJackson2HttpMessageConverter.java
@@ -1,17 +1,16 @@
 package com.system.management.customer.serialization.converter;
 
-import static com.system.management.customer.util.MediaType.*;
+import static com.system.management.customer.util.MediaType.APPLICATION_YAML;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 public class YamlJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
 
-	protected YamlJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+	public YamlJackson2HttpMessageConverter() {
 		super(
 				new YAMLMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL),
 				MediaType.parseMediaType(APPLICATION_YAML)
