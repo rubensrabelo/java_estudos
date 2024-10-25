a57a7a880d197bc1df5703ffb9bddfa5261eb992
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-10-25 08:35:43 -0300

    Adding yaml serialization in the customer project

---
+++ projects/beginner/customer/customer/src/main/java/com/system/management/customer/serialization/converter/YamlJackson2HttpMessageConverter.java
@@ -0,0 +1,21 @@
+package com.system.management.customer.serialization.converter;
+
+import static com.system.management.customer.util.MediaType.*;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+
+public class YamlJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+
+	protected YamlJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+		super(
+				new YAMLMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL),
+				MediaType.parseMediaType(APPLICATION_YAML)
+				);
+	}
+
+}
