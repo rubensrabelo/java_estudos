e6e2b88e282c6cb72cae10166305e10c01d55f6e
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-10-30 14:34:01 -0300

    Understaing test erro in swagger

---
+++ projects/beginner/customer/customer/src/test/java/com/system/management/customer/configs/TestConfigs.java
@@ -0,0 +1,16 @@
+package com.system.management.customer.configs;
+
+public class TestConfigs {
+	
+	public static final int SERVER_PORT = 8888;
+	
+	public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
+	public static final String HEADER_PARAM_ORIGIN = "Origin";
+	
+	public static final String CONTENT_TYPE_JSON = "application/json";
+	public static final String CONTENT_TYPE_XML = "application/xml";
+	public static final String CONTENT_TYPE_YML = "application/x-yaml";
+
+	public static final String ORIGIN_LOCALHOST_8080 = "http://localhost:8080";
+	public static final String ORIGIN_LOCALHOST_5000 = "http://localhost:5000";
+}
