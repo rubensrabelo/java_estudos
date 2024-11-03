c5e92a6d144dc7f74e1bc54f77f05068cfa9fa03
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-03 08:22:17 -0300

    Implementing serializable in user model

--- course/product/product/src/main/java/com/project/product/model/User.java
+++ course/product/product/src/main/java/com/project/product/model/User.java
@@ -1,9 +1,12 @@
 package com.project.product.model;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class User {
-	
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
 	private Long id;
 	private String name;
 	private String email;
