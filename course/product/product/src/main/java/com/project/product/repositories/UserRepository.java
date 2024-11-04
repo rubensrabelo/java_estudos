471a3e3e6723086451b91f194224e6700b954350
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-04 13:46:51 -0300

    Putting repository annotations in UserRepository

--- course/product/product/src/main/java/com/project/product/repositories/UserRepository.java
+++ course/product/product/src/main/java/com/project/product/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.project.product.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.project.product.models.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
