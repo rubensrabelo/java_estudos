9526a866175e72af8cecea1cfc8555dd464b33dc
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-10 10:07:15 -0300

    Adding create, delete and update methods in the user service

--- course/product/product/src/main/java/com/project/product/services/UserService.java
+++ course/product/product/src/main/java/com/project/product/services/UserService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.project.product.models.User;
 import com.project.product.repositories.UserRepository;
+import com.project.product.services.exceptions.DatabaseException;
+import com.project.product.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -23,5 +29,35 @@ public class UserService {
 		Optional<User> entity = repository.findById(id);
 		
 		return  entity.get();
+	}
+	
+	public User create(User entity) {
+		return repository.save(entity);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public User update(Long id, User obj) {
+		try {
+			User entity = repository.findById(id).get();
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
