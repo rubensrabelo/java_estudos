0ff3656da1d5e2dbc5c073495cad2e034fbdd0a2
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-10 18:41:35 -0300

    Adding category model

---
+++ projects/beginner/book/book/src/main/java/com/management/book/models/Category.java
@@ -0,0 +1,66 @@
+package com.management.book.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String description;
+	
+	public Category() {
+		super();
+	}
+
+	public Category(Long id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(description, other.description) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name);
+	}
+}
