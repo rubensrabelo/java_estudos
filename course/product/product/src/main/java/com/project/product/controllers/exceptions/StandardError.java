f460a28bcf23670d0f0536abd9878374e6442891
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-10 10:08:37 -0300

    Adding StardandError and starting handle in the user controller

---
+++ course/product/product/src/main/java/com/project/product/controllers/exceptions/StandardError.java
@@ -0,0 +1,66 @@
+package com.project.product.controllers.exceptions;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class StandardError implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Instant timestamp;
+	private String status;
+	private String error;
+	private String message;
+	private String path;	
+	
+	public StandardError() {
+	}
+
+	public StandardError(Instant timestamp, String status, String error, String message, String path) {
+		this.timestamp = timestamp;
+		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.path = path;
+	}
+
+	public Instant getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Instant timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+}
