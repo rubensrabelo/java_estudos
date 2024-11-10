686082722024f8546f653f6ae250c87fb5f4b006
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-10 18:33:00 -0300

    Removing old folders

--- projects/beginner/customer/customer/src/main/resources/db/migration/V2__Insert_Into_Table_Customer.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO customer (first_name, last_name, email, phone_number, birth_date, gender) 
-VALUES 
-('John', 'Doe', 'john.doe@example.com', '123-456-7890', '1990-05-15', 'M'),
-('Jane', 'Smith', 'jane.smith@example.com', '987-654-3210', '1985-08-20', 'F'),
-('Michael', 'Johnson', 'michael.johnson@example.com', '555-123-4567', '1992-12-03', 'M'),
-('Emily', 'Davis', 'emily.davis@example.com', '444-987-6543', '1995-03-12', 'F'),
-('David', 'Brown', 'david.brown@example.com', '222-654-7890', '1988-07-25', 'M');
--- projects/beginner/todolist/todolist/src/main/resources/db/migration/V1__Create_Table_Task.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS public.task (
-    id SERIAL PRIMARY KEY,
-    created_at TIMESTAMP(6) WITHOUT TIME ZONE,
-    description VARCHAR(255) COLLATE pg_catalog."default",
-    name VARCHAR(255) COLLATE pg_catalog."default",
-    task_status INTEGER,
-    updated_at TIMESTAMP(6) WITHOUT TIME ZONE
-);
