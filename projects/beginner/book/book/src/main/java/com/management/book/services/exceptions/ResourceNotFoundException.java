91902cb3cce6a679551ba9215778385802a604ef
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-11 19:06:28 -0300

    Creating ResourceNotFoundException

---
+++ projects/beginner/book/book/src/main/java/com/management/book/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.management.book.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource with id=" + id + " not found.");
+	}
+}
