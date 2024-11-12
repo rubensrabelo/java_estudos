483d4b2a3f21f37840e2e57a94e0dad1af77a736
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-12 19:12:02 -0300

    Finishing author controller

--- projects/beginner/book/book/src/main/java/com/management/book/controllers/AuthorController.java
+++ projects/beginner/book/book/src/main/java/com/management/book/controllers/AuthorController.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -35,11 +39,26 @@ public class AuthorController {
 		return ResponseEntity.ok().body(entity);
 	}
 	
-	public ResponseEntity<Author> create(Author obj) {
+	@PostMapping
+	public ResponseEntity<Author> create(@RequestBody Author obj) {
 		obj = service.create(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(obj.getId()).toUri();
 		
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(Long id) {
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Author> update(@PathVariable Long id, @RequestBody Author obj) {
+		obj = service.update(id, obj);
+		
+		return ResponseEntity.ok().body(obj);
 	}
 }
