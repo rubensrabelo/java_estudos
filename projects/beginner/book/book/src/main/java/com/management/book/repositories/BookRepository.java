196360d8fb85d2824defb184087a726b3aef10a1
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-14 08:06:38 -0300

    Creating book repository

---
+++ projects/beginner/book/book/src/main/java/com/management/book/repositories/BookRepository.java
@@ -0,0 +1,16 @@
+package com.management.book.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.management.book.models.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+	
+	List<Book> findByTitleContainingIgnoreCase(String title);
+	
+	Book findByIsbn(String isbn);
+	
+	List<Book> findByAuthorName(String authorName);
+}
