4509ca798c06478ca022ad3690c0c02cb6f87427
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-16 11:47:49 -0300

    Adding create method in book service

--- projects/beginner/book/book/src/main/java/com/management/book/repositories/BookRepository.java
+++ projects/beginner/book/book/src/main/java/com/management/book/repositories/BookRepository.java
@@ -1,14 +1,8 @@
 package com.management.book.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.management.book.models.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-	
-	List<Book> findByTitleContainingIgnoreCase(String title);
-	
-	List<Book> findByAuthorName(String authorName);
 }
--- projects/beginner/book/book/src/main/java/com/management/book/services/BookService.java
+++ projects/beginner/book/book/src/main/java/com/management/book/services/BookService.java
@@ -2,11 +2,16 @@ package com.management.book.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.management.book.models.Author;
 import com.management.book.models.Book;
+import com.management.book.models.Category;
+import com.management.book.repositories.AuthorRepository;
 import com.management.book.repositories.BookRepository;
 import com.management.book.services.exceptions.ResourceNotFoundException;
 
@@ -14,15 +19,41 @@ import com.management.book.services.exceptions.ResourceNotFoundException;
 public class BookService {
 	
 	@Autowired
-	private BookRepository repository;
+	private BookRepository bookRepository;
+	
+	@Autowired
+	private AuthorRepository authorRepository;
+	
+	@Autowired
+	private CategoryService categoryRepository;
 	
 	public List<Book> findAll() {
-		return repository.findAll();
+		return bookRepository.findAll();
 	}
 	
 	public Book findById(Long id) {
-		Optional<Book> entity = repository.findById(id);
+		Optional<Book> entity = bookRepository.findById(id);
 		
 		return entity.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public Book create(Book book) {
+		Author author = authorRepository.findById(book.getAuthor().getId())
+				.orElseThrow(() -> new ResourceNotFoundException(book.getAuthor().getId()));
+		
+		Set<Category> validCategories = book.getCategories().stream()
+				.map(cat -> {
+					Category c = categoryRepository.findById(cat.getId());
+					if(c == null)
+						throw new ResourceNotFoundException(cat.getId());
+					return c;
+				}).collect(Collectors.toSet());
+		
+		book.setAuthor(author);
+		
+		for(Category cat : validCategories)
+				book.addCategory(cat);
+		
+		return bookRepository.save(book);
 	}
 }
