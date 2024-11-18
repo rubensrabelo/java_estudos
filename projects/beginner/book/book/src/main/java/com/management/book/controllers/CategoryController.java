7b4e229a34f91e9a1eca50955ddc54f9041d69c9
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-18 14:59:41 -0300

    Inserting openAPI annotation in the category controller

--- projects/beginner/book/book/src/main/java/com/management/book/controllers/CategoryController.java
+++ projects/beginner/book/book/src/main/java/com/management/book/controllers/CategoryController.java
@@ -18,20 +18,42 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.management.book.models.Category;
 import com.management.book.services.CategoryService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping(value = "/categories")
+@Tag(name = "Category", description = "Controller to manage book category")
 public class CategoryController {
 	
 	@Autowired
 	private CategoryService service;
 	
 	@GetMapping
+	@Operation(summary = "Finds all Categories", description = "Finds all Categories", tags = { "Category" }, responses = {
+			@ApiResponse(description = "Sucess", responseCode = "200", content = {
+					@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Category.class))) }),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content), })
 	public ResponseEntity<List<Category>> findAll() {
 		List<Category> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
+	@Operation(summary = "Finds a Category", description = "Finds a Category", tags = { "Category" }, responses = {
+			@ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = Category.class))),
+			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content), })
 	public ResponseEntity<Category> findById(@PathVariable Long id) {
 		Category entity = service.findById(id);
 		
@@ -39,6 +61,12 @@ public class CategoryController {
 	}
 	
 	@PostMapping
+	@Operation(summary = "Adds a new Category", description = "Adds a new Category by passing in a JSON representation of the category!", tags = {
+	"Category" }, responses = {
+			@ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = Category.class))),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content), })
 	public ResponseEntity<Category> create(@RequestBody Category obj) {
 		obj = service.create(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
@@ -48,6 +76,13 @@ public class CategoryController {
 	}
 	
 	@DeleteMapping(value = "/{id}")
+	@Operation(summary = "Deletes a Category", description = "Deletes a Category by passing in a JSON representation of the category!", tags = {
+	"Author" }, responses = {
+			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content), })
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		
@@ -55,6 +90,13 @@ public class CategoryController {
 	}
 	
 	@PutMapping(value = "/{id}")
+	@Operation(summary = "Updates a Category", description = "Updates a Category by passing in a JSON representation of the Category!", tags = {
+	"Category" }, responses = {
+			@ApiResponse(description = "Updated", responseCode = "200", content = @Content(schema = @Schema(implementation = Category.class))),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content), })
 	public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category obj) {
 		obj = service.update(id, obj);
 		
