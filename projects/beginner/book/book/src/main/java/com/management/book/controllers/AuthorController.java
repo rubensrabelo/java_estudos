8013e85bc4b35abb794905a3e77604b94c667d1f
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-18 14:55:35 -0300

    Inserting openAPI annotation in the author controller

--- projects/beginner/book/book/src/main/java/com/management/book/controllers/AuthorController.java
+++ projects/beginner/book/book/src/main/java/com/management/book/controllers/AuthorController.java
@@ -18,47 +18,88 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.management.book.models.Author;
 import com.management.book.services.AuthorService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping(value = "/authors")
+@Tag(name = "Author", description = "Controller to manage book author")
 public class AuthorController {
-	
+
 	@Autowired
 	private AuthorService service;
-	
+
 	@GetMapping
+	@Operation(summary = "Finds all authors", description = "Finds all authors", tags = { "Author" }, responses = {
+			@ApiResponse(description = "Sucess", responseCode = "200", content = {
+					@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Author.class))) }),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content), })
 	public ResponseEntity<List<Author>> findAll() {
 		List<Author> list = service.findAll();
-		
+
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
+	@Operation(summary = "Finds a Author", description = "Finds a Author", tags = { "Author" }, responses = {
+			@ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = Author.class))),
+			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content), })
 	public ResponseEntity<Author> findById(@PathVariable Long id) {
 		Author entity = service.findByID(id);
-		
+
 		return ResponseEntity.ok().body(entity);
 	}
-	
+
 	@PostMapping
+	@Operation(summary = "Adds a new Author", description = "Adds a new Author by passing in a JSON representation of the author!", tags = {
+			"Author" }, responses = {
+					@ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = Author.class))),
+					@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+					@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+					@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content), })
 	public ResponseEntity<Author> create(@RequestBody Author obj) {
 		obj = service.create(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(obj.getId()).toUri();
-		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
 		return ResponseEntity.created(uri).body(obj);
 	}
-	
+
 	@DeleteMapping(value = "/{id}")
+	@Operation(summary = "Deletes a Author", description = "Deletes a Author by passing in a JSON representation of the author!", tags = {
+			"Author" }, responses = {
+					@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+					@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+					@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+					@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+					@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content), })
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
-		
+
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@PutMapping(value = "/{id}")
+	@Operation(summary = "Updates a Author", description = "Updates a Author by passing in a JSON representation of the author!", tags = {
+			"Author" }, responses = {
+					@ApiResponse(description = "Updated", responseCode = "200", content = @Content(schema = @Schema(implementation = Author.class))),
+					@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+					@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+					@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+					@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content), })
 	public ResponseEntity<Author> update(@PathVariable Long id, @RequestBody Author obj) {
 		obj = service.update(id, obj);
-		
+
 		return ResponseEntity.ok().body(obj);
 	}
 }
