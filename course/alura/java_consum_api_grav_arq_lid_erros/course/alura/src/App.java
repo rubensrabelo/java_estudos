485a65ca2eecebefe0dfe8ffe55fecc2d437dc68
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-23 20:15:52 -0300

    Improving alura project

--- course/alura/java_consum_api_grav_arq_lid_erros/course/alura/src/App.java
+++ course/alura/java_consum_api_grav_arq_lid_erros/course/alura/src/App.java
@@ -4,6 +4,10 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
+import com.google.gson.Gson;
+
+import models.Title;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
@@ -11,7 +15,7 @@ public class App {
         System.out.println("Insert a movie for search: ");
         var search = sc.nextLine();
 
-        String url = "";
+        String url = "https://www.omdbapi.com/?t=" + search + "&apikey=802bd4ce";
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -19,8 +23,17 @@ public class App {
                 .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        
+        String json = response.body();
 
-        System.out.println(response.body());
+        // System.out.println(json);
+
+
+        Gson gson = new Gson();
+
+        Title title = gson.fromJson(json, Title.class);
+
+        System.out.println(title);
 
         sc.close();
     }
---
+++ course/alura/java_consum_api_grav_arq_lid_erros/course/alura/src/models/Title.java
@@ -0,0 +1,81 @@
+package models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Title implements Comparable<Title> {
+    
+    @SerializedName("Title")
+    private String nome;
+
+    @SerializedName("Year")
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    public Title(String nome, int anoDeLancamento) {
+        this.nome = nome;
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    public void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+
+    @Override
+    public int compareTo(Title other) {
+        return this.getNome().compareTo(other.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "Title [nome=" + nome + ", anoDeLancamento=" + anoDeLancamento + "]";
+    }
+}
