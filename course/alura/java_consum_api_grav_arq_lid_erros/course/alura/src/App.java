4ff3e12776512d5f7a0d5794657597e14c84a9f2
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-23 17:41:04 -0300

    Creating api first program

---
+++ course/alura/java_consum_api_grav_arq_lid_erros/course/alura/src/App.java
@@ -0,0 +1,27 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insert a movie for search: ");
+        var search = sc.nextLine();
+
+        String url = "https://www.omdbapi.com/?t=" + search + "&apikey=802bd4ce";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+        sc.close();
+    }
+}
