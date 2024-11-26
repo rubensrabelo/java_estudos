3c8526ba4dade53cd08f0240b0e6f7227d140851
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-26 13:42:37 -0300

    Removing auth-jwt folder

--- course/auth-jwt/study/study/src/main/java/com/example/study/domain/product/ProductResponseDTO.java
+++
@@ -1,8 +0,0 @@
-package com.example.study.domain.product;
-
-public record ProductResponseDTO(String id, String name, Integer price) {
-
-	public ProductResponseDTO(Product product) {
-		this(product.getId(), product.getName(), product.getPrice());
-	}	
-}
