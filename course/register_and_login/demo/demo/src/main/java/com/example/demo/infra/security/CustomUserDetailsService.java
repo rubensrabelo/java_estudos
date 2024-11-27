43821d47c5473d00d14224acf1f448eabbe3650a
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-27 15:10:06 -0300

    Improving login and register program

---
+++ course/register_and_login/demo/demo/src/main/java/com/example/demo/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.demo.infra.security;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.domain.user.User;
+import com.example.demo.repositories.UserRepository;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = this.repository.findByEmail(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User not found."));
+		return new org.springframework.security.core.userdetails.User(
+					user.getEmail(),
+					user.getPassword(),
+					new ArrayList<>()
+				);
+	}
+	
+	
+}
