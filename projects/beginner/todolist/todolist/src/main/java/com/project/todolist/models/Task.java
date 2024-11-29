09a74cc28a977ef516f4a6adab0abbc4f156d9cb
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-29 12:27:07 -0300

    Starting project structure and models

---
+++ projects/beginner/todolist/todolist/src/main/java/com/project/todolist/models/Task.java
@@ -0,0 +1,12 @@
+package com.project.todolist.models;
+
+import java.time.LocalDate;
+
+public class Task {
+	
+	private Long id;
+	private String title;
+	private String description;
+	private LocalDate create_at;
+	private LocalDate update_at;
+}
