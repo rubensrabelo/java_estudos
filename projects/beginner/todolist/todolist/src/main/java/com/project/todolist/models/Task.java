0f24f39c7fa6d1e23c997496cff4ec5eeb92b323
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-29 12:39:40 -0300

    Finishing task models

--- projects/beginner/todolist/todolist/src/main/java/com/project/todolist/models/Task.java
+++ projects/beginner/todolist/todolist/src/main/java/com/project/todolist/models/Task.java
@@ -1,12 +1,113 @@
 package com.project.todolist.models;
 
+import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Objects;
 
-public class Task {
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.project.todolist.models.enums.TaskStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class Task implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	private String description;
-	private LocalDate create_at;
-	private LocalDate update_at;
+	
+	@CreationTimestamp
+	@Column(name = "created_at", updatable = false, nullable = false)
+	private LocalDate createdAt;
+	
+	@UpdateTimestamp
+	@Column(name = "updated_at")
+	private LocalDate updatedAt;
+	
+	private Integer status;
+
+	public Task() {
+	}
+
+	public Task(Long id, String title, String description, LocalDate createdAt, LocalDate updatedAt,
+			TaskStatus status) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		setStatus(status);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getCreatedAt() {
+		return createdAt;
+	}
+
+	public LocalDate getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public TaskStatus getStatus() {
+		return TaskStatus.valueOf(status);
+	}
+
+	public void setStatus(TaskStatus taskStatus) {
+		if(taskStatus != null)
+			this.status = taskStatus.getCode();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(createdAt, description, id, status, title, updatedAt);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Task other = (Task) obj;
+		return Objects.equals(createdAt, other.createdAt) && Objects.equals(description, other.description)
+				&& Objects.equals(id, other.id) && Objects.equals(status, other.status)
+				&& Objects.equals(title, other.title) && Objects.equals(updatedAt, other.updatedAt);
+	}
 }
