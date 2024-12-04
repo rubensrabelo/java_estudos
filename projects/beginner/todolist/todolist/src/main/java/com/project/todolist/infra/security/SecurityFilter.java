0ba2af6a80a43d17cee6817aeecd85d447199fe4
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-12-04 12:05:31 -0300

    Finishing security filter

--- projects/beginner/todolist/todolist/src/main/java/com/project/todolist/infra/security/SecurityFilter.java
+++ projects/beginner/todolist/todolist/src/main/java/com/project/todolist/infra/security/SecurityFilter.java
@@ -1,8 +1,12 @@
 package com.project.todolist.infra.security;
 
 import java.io.IOException;
+import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -31,16 +35,18 @@ public class SecurityFilter extends OncePerRequestFilter {
 		
 		if(login != null) {
 			User user = userRepository.findByEmail(login)
-					.orElseThrow(() -> new RuntimeException("Troca isso"));
-			
+					.orElseThrow(() -> new RuntimeException("User not found"));
+			var authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+			var authentication = new UsernamePasswordAuthenticationToken(user, null, authorities);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
 		}
+		
+		filterChain.doFilter(request, response);
 	}
 
 	private String recoverToken(HttpServletRequest request) {
 		var authHeader = request.getHeader("Authorization");
 		if(authHeader == null) return null;
 		return authHeader;
-	}
-	
-	
+	}	
 }
