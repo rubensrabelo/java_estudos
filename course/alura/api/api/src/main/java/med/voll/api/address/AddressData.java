c1af99bc805bb43da9030631a6f608bac1e5b033
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-10 16:39:53 -0300

    Alura project med

---
+++ course/alura/api/api/src/main/java/med/voll/api/address/AddressData.java
@@ -0,0 +1,14 @@
+package med.voll.api.address;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record AddressData(
+		@NotBlank String street,
+		@NotBlank String neighborhood,
+		@NotBlank @Pattern(regexp = "\\d{8}") String postalCode,
+		String number,
+		String additionalInfo,
+		@NotBlank String city,
+		@NotBlank String state) {
+}
