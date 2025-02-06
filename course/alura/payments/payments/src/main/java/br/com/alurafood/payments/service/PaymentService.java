12a03c3613e9fb23e2b9d6f4512f2aba9f879837
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-06 14:44:30 -0300

    Starting payment service

---
+++ course/alura/payments/payments/src/main/java/br/com/alurafood/payments/service/PaymentService.java
@@ -0,0 +1,14 @@
+package br.com.alurafood.payments.service;
+
+import br.com.alurafood.payments.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    
+}
