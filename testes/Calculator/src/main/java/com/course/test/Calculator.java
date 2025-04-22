633857ef49d193fd868ed34374c07e43fc1f882d
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-04-22 13:36:34 -0300

    test(example): study basics about junit

--- testes/Calculator/src/main/java/com/course/test/Calculator.java
+++ testes/Calculator/src/main/java/com/course/test/Calculator.java
@@ -5,4 +5,6 @@ public class Calculator {
     public int division(int a, int b) {
         return a / b;
     }
+
+    public int subtraction(int a, int b) {return a - b;}
 }
--- testes/Calculator/src/test/java/com/course/test/CalculatorTest.java
+++ testes/Calculator/src/test/java/com/course/test/CalculatorTest.java
@@ -7,6 +7,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
-    void division() {
+    void integerDivision() {
+        Calculator calculator = new Calculator();
+        int result = calculator.division(4, 2);
+
+        assertEquals(2, result, "4/2 did not produce 2");
+    }
+
+    @Test
+    void integerSubtraction() {
+        Calculator calculator = new Calculator();
+        int a = 4;
+        int b = 2;
+        int expectedResult = a - b;
+        int actualResult = calculator.subtraction(a, b);
+        assertEquals(expectedResult, actualResult,
+                ()->a + " - " + b + " did not produce " + actualResult);
     }
 }
