1c2e7662eb604ebd74a780942018036249471e71
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-04-23 12:43:47 -0300

    test(basics): finish section 05

--- testes/Calculator/src/test/java/com/course/test/CalculatorTest.java
+++ testes/Calculator/src/test/java/com/course/test/CalculatorTest.java
@@ -1,14 +1,24 @@
 package com.course.test;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Test math operations in calculator class")
 class CalculatorTest {
 
+    static Calculator calculator;
+
+    @BeforeAll
+    static void setup() {
+        calculator = new Calculator();
+    }
+
+    @DisplayName("Test 4/2 = 2")
     @Test
-    void integerDivision() {
-        Calculator calculator = new Calculator();
+    void testDivision_WhenFourIsDividesByTwo_ShouldReturnTwo() {
         int result = calculator.division(4, 2);
 
         assertEquals(2, result, "4/2 did not produce 2");
@@ -16,7 +26,6 @@ class CalculatorTest {
 
     @Test
     void integerSubtraction() {
-        Calculator calculator = new Calculator();
         int a = 4;
         int b = 2;
         int expectedResult = a - b;
