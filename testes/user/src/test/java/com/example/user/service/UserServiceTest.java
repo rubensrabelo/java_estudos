eb9f5b149edb9c7c8b4b87edc518c44c66251085
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-04-28 13:21:02 -0300

    test(test): start with tdd

---
+++ testes/user/src/test/java/com/example/user/service/UserServiceTest.java
@@ -0,0 +1,16 @@
+package com.example.user.service;
+
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+
+	@Test
+	void testCreateUser_whenUserDetailsProvided_returnUserObject() {
+		// Arrange
+		UserService userService = new UserServiceImpl();
+
+		// Act
+
+		// Assert
+	}
+}
