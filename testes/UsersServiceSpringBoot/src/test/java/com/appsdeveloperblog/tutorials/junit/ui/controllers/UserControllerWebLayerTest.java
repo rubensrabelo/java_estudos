2a6286c07c7a4351879072231fed2181c4fc4276
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-05-13 12:10:30 -0300

    test(user): mock service layer

--- testes/UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UserControllerWebLayerTest.java
+++ testes/UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UserControllerWebLayerTest.java
@@ -1,18 +1,72 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import com.appsdeveloperblog.tutorials.junit.service.UsersService;
+import com.appsdeveloperblog.tutorials.junit.service.UsersServiceImpl;
+import com.appsdeveloperblog.tutorials.junit.shared.UserDto;
+import com.appsdeveloperblog.tutorials.junit.ui.request.UserDetailsRequestModel;
+import com.appsdeveloperblog.tutorials.junit.ui.response.UserRest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @WebMvcTest(
         controllers = UsersController.class,
         excludeAutoConfiguration = {SecurityAutoConfiguration.class}
 )
+@MockBean({UsersServiceImpl.class})
 public class UserControllerWebLayerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    UsersService usersService;
 
     @DisplayName("User can be created")
     @Test
-    void testCreateUser_whenValidUserDetailsProvided_returnsCreatedUserDetails() {}
+    void testCreateUser_whenValidUserDetailsProvided_returnsCreatedUserDetails() throws Exception {
+        // Arrange
+        UserDetailsRequestModel userDetailsRequestModel = new UserDetailsRequestModel();
+        userDetailsRequestModel.setFirstName("John");
+        userDetailsRequestModel.setLastName("Doe");
+        userDetailsRequestModel.setEmail("john@doe.com");
+        userDetailsRequestModel.setPassword("password");
+        userDetailsRequestModel.setRepeatPassword("password");
+
+        UserDto userDto = new ModelMapper().map(userDetailsRequestModel, UserDto.class);
+        userDto.setUserId(UUID.randomUUID().toString());
+        when(usersService.createUser(any(UserDto.class))).thenReturn(userDto);
+
+        RequestBuilder request = MockMvcRequestBuilders.post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(userDetailsRequestModel));
+
+        // Act
+        MvcResult mvcResult = mockMvc.perform(request).andReturn();
+        String responseString = mvcResult.getResponse().getContentAsString();
+        UserRest createdUser = new ObjectMapper().readValue(responseString, UserRest.class);
+
+        // Asserts
+        Assertions.assertEquals(userDetailsRequestModel.getFirstName(), createdUser.getFirstName(), "First Name not equals");
+        Assertions.assertEquals(userDetailsRequestModel.getLastName(), createdUser.getLastName(), "Last Name not equals");
+        Assertions.assertEquals(userDetailsRequestModel.getEmail(), createdUser.getEmail(), "Email not equals");
+        Assertions.assertFalse(createdUser.getUserId().isEmpty(), "UserId should not be empty");
+    }
 }
