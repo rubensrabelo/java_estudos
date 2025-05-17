ca97e10a3a025745373db76b53373016e9e9b03c
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-05-17 16:30:34 -0300

    test(integration): start integration test with user controller

---
+++ testes/UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -0,0 +1,29 @@
+package com.appsdeveloperblog.tutorials.junit.ui.controllers;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UsersControllerIntegrationTest {
+
+    @Value("${server.port}")
+    private int serverPort;
+
+    @LocalServerPort
+    private int localServerPort;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    private String authorizationToken;
+
+
+}
